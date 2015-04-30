485eaa1c17295bf4f975bc52d43306a37b2db99d
guangbin <guangbin79@gmail.com>
2015-04-30 11:55:21 +0800

    modify xdebug.cpp

--- src/Qt/xdebug/xdebug.cpp
+++ src/Qt/xdebug/xdebug.cpp
@@ -7,9 +7,9 @@ void xdebug_assert(xbool_t exp) {
     Q_ASSERT(exp);
 }
 
-void xdebug_printf(const xchar_t * str) {
+void xdebug_printf(const char * format, va_list ap) {
     QTextStream out(stdout);
 
-    out << QString((const char *)str);
+    out << QString().vsprintf(format, ap);
 }
 
