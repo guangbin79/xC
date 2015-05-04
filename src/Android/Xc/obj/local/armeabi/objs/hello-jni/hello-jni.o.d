b95e4363f60facb767dc80d2b55d0c0852de14d3
wangxt <wangxt@tiros.com.cn>
2015-05-04 10:30:29 +0800

    添加android工程

---
+++ src/Android/Xc/obj/local/armeabi/objs/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/hello-jni/hello-jni.o: jni/hello-jni.c \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/string.h:
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/Users/wangxiongtao/develop/android-ndk-r8e/platforms/android-14/arch-arm/usr/include/jni.h:
---
+++ src/Android/Xc/obj/local/armeabi/objs/xdebug/xdebug.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi/objs/xdebug/xdebug.o: jni/xdebug.c \
+ jni/../../../../xdebug.h jni/../../../.././xboolean.h \
+ jni/../../../../././xinteger.h jni/../../../.././xcharacter.h \
+ jni/../../../../xvarargs.h jni/../../../.././xinteger.h \
+ jni/../../../.././xpointer.h
+
+jni/../../../../xdebug.h:
+
+jni/../../../.././xboolean.h:
+
+jni/../../../../././xinteger.h:
+
+jni/../../../.././xcharacter.h:
+
+jni/../../../../xvarargs.h:
+
+jni/../../../.././xinteger.h:
+
+jni/../../../.././xpointer.h:
